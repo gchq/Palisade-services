f73df74792293d60d0354af1eed437c48100f636
dev930018 <56113485+dev930018@users.noreply.github.com>
2019-10-21 11:52:05 +0100

    Proper Eureka configuration for audit-service

--- audit-service/src/main/java/uk/gov/gchq/palisade/service/audit/AuditApplication.java
+++ audit-service/src/main/java/uk/gov/gchq/palisade/service/audit/AuditApplication.java
@@ -22,14 +22,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@EnableDiscoveryClient
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@EnableEurekaClient
 @SpringBootApplication
 public class AuditApplication {
 
@@ -46,9 +48,10 @@ class ServiceInstanceRestController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
-    @RequestMapping("/service-instances/{applicationName}")
+    @RequestMapping(value = "/service-instances/{applicationName}", method = GET)
     public List<ServiceInstance> serviceInstancesByApplicationName(
             @PathVariable String applicationName) {
         return this.discoveryClient.getInstances(applicationName);
     }
+
 }
