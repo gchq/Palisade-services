6e2f8271e3529d3aca1bd533f7336a0b23ed47ef
NigelW <nigel.warburton@digital.ncsc.gov.uk>
2019-10-30 08:20:47 +0000

    PAL-218 delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm $PWD/mvn_dependency_tree.txt
-dt=$(date '+%d/%m/%Y %H:%M:%S');
-echo "$dt" > $PWD/mvn_dependency_tree.txt
-mvn dependency:tree -DoutputFile=$PWD/mvn_dependency_tree.txt -DappendOutput=true
-mvn install
