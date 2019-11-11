85ba1603f43eefa2b4525bba587c493963a16ea1
pd104923 <54805408+pd104923@users.noreply.github.com>
2019-11-11 08:26:41 +0000

    Pal 236 add error handler and details

--- data-service/src/main/java/uk/gov/gchq/palisade/service/data/exception/ErrorDetails.java
+++ data-service/src/main/java/uk/gov/gchq/palisade/service/data/exception/ErrorDetails.java
@@ -1,4 +1,43 @@
+/*
+ * Copyright 2019 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.gchq.palisade.service.data.exception;
 
+import uk.gov.gchq.palisade.service.request.Request;
+
+import java.util.Date;
+
+import static java.util.Objects.requireNonNull;
+
 public class ErrorDetails {
+
+    private Date date;
+    private String message;
+    private Request details;
+
+    public ErrorDetails() {
+
+    }
+
+    public ErrorDetails(Date date, String message, Request details) {
+        requireNonNull(date, "Date cannot be null");
+        requireNonNull(message, "Message cannot be null");
+        requireNonNull(details, "Details cannot be null");
+        this.date = date;
+        this.message = message;
+        this.details = details;
+    }
 }
