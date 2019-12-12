7388454725007f2cde8e69d8c90c1f396860ca86
NigelW <nigel.warburton@digital.ncsc.gov.uk>
2019-12-12 17:04:29 +0000

    PAL-264 revert SimpleCacheService

--- palisade-service/src/main/java/uk/gov/gchq/palisade/service/palisade/repository/SimpleCacheService.java
+++ palisade-service/src/main/java/uk/gov/gchq/palisade/service/palisade/repository/SimpleCacheService.java
@@ -35,6 +35,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static java.util.Objects.requireNonNull;
@@ -210,27 +211,18 @@ public class SimpleCacheService implements CacheService {
     public <V> CompletableFuture<Optional<V>> get(final GetCacheRequest<V> request) {
         requireNonNull(request, "request");
         //make final key name
-        LOGGER.info("SimpleCacheService get");
         String baseKey = request.makeBaseName();
         LOGGER.info("Get item with key {}", baseKey);
 
-//        Supplier<Optional<V>> getFunction = () -> {
-//            SimpleCacheObject result = doCacheRetrieve(baseKey, maxLocalTTL);
-//            LOGGER.debug("Retrieved {} from cache with result {}", baseKey, result);
-//
-//            //assign so Javac can infer the generic type
-//            BiFunction<byte[], Class<V>, V> decode = codecs.getValueDecoder((Class<V>) result.getValueClass());
-//            return result.getValue().map(x -> decode.apply(x, (Class<V>) result.getValueClass()));
-//        };
-        return CompletableFuture.supplyAsync(() -> {
-            LOGGER.info("supply async function");
+        Supplier<Optional<V>> getFunction = () -> {
             SimpleCacheObject result = doCacheRetrieve(baseKey, maxLocalTTL);
             LOGGER.info("Retrieved {} from cache with result {}", baseKey, result);
 
             //assign so Javac can infer the generic type
             BiFunction<byte[], Class<V>, V> decode = codecs.getValueDecoder((Class<V>) result.getValueClass());
             return result.getValue().map(x -> decode.apply(x, (Class<V>) result.getValueClass()));
-        });
+        };
+        return CompletableFuture.supplyAsync(getFunction);
     }
 
     /**
