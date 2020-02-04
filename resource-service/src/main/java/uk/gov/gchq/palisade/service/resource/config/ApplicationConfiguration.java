5acf49ce57d4f5ca56ec8268a8b836b156367d3a
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-02-04 14:00:55 +0000

    Pal 390 deleted cache repository classes, cache service interface, removed backing store from resource-service bean

--- resource-service/src/main/java/uk/gov/gchq/palisade/service/resource/config/ApplicationConfiguration.java
+++ resource-service/src/main/java/uk/gov/gchq/palisade/service/resource/config/ApplicationConfiguration.java
@@ -19,34 +19,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.hadoop.conf.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import uk.gov.gchq.palisade.jsonserialisation.JSONSerialiser;
-import uk.gov.gchq.palisade.service.resource.exception.ApplicationAsyncExceptionHandler;
-import uk.gov.gchq.palisade.service.resource.repository.BackingStore;
-import uk.gov.gchq.palisade.service.resource.repository.EtcdBackingStore;
-import uk.gov.gchq.palisade.service.resource.repository.HashMapBackingStore;
-import uk.gov.gchq.palisade.service.resource.repository.K8sBackingStore;
-import uk.gov.gchq.palisade.service.resource.repository.PropertiesBackingStore;
-import uk.gov.gchq.palisade.service.resource.repository.SimpleCacheService;
-import uk.gov.gchq.palisade.service.resource.service.CacheService;
 import uk.gov.gchq.palisade.service.resource.service.HadoopResourceService;
-import uk.gov.gchq.palisade.service.resource.web.ServiceInstanceRestController;
 
 import java.io.IOException;
-import java.net.URI;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.Executor;
-
-import static java.util.stream.Collectors.toList;
 
 /**
  * Bean configuration and dependency injection graph
@@ -57,7 +40,7 @@ public class ApplicationConfiguration implements AsyncConfigurer {
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationConfiguration.class);
 
     @Bean
-    public HadoopResourceService resourceService(final Configuration config, final Map<String, BackingStore> backingStore) throws IOException {
+    public HadoopResourceService resourceService(final Configuration config) throws IOException {
         return new HadoopResourceService(config);
     }
 
