ce14c6238550d85e89965c4d034b6a1b597c99a5
developer01189998819991197253 <ChrisCoDev@gmail.com>
2020-02-10 13:04:44 +0000

    pal-389-added nullUserServiceTests

---
+++ user-service/src/test/java/uk/gov/gchq/palisade/service/user/service/NullUserServiceTest.java
@@ -0,0 +1,41 @@
+package uk.gov.gchq.palisade.service.user.service;
+
+import org.junit.Test;
+import org.testcontainers.shaded.org.apache.commons.lang.ObjectUtils.Null;
+
+import uk.gov.gchq.palisade.User;
+import uk.gov.gchq.palisade.service.user.exception.NoSuchUserIdException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/*
+ * Copyright 2020 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */public class NullUserServiceTest {
+     NullUserService nullUserService = new NullUserService();
+
+    @Test(expected = NoSuchUserIdException.class)
+    public void getUser() {
+        User user = new User().userId("testUser");
+        nullUserService.getUser(user.getUserId());
+    }
+
+    @Test
+    public void addUser() {
+        User user = new User().userId("testUser");
+        User actual = nullUserService.addUser(user);
+        assertThat(user, is(actual));
+    }
+}
