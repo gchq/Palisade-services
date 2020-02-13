0d13bcc5e28491b8b375d9f6e1dc86d4daedd204
dev-958 <wolfie.acb@gmail.com>
2020-02-13 15:32:20 +0000

    added repositories and entity type converters

--- palisade-service/src/main/java/uk/gov/gchq/palisade/service/palisade/domain/DataRequestEntity.java
+++ palisade-service/src/main/java/uk/gov/gchq/palisade/service/palisade/domain/DataRequestEntity.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2020 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.gov.gchq.palisade.service.palisade.domain;
 
 import uk.gov.gchq.palisade.Context;
+import uk.gov.gchq.palisade.RequestId;
 import uk.gov.gchq.palisade.User;
 import uk.gov.gchq.palisade.resource.LeafResource;
 import uk.gov.gchq.palisade.rule.Rules;
+import uk.gov.gchq.palisade.service.request.DataRequestConfig;
 
 import javax.persistence.Column;
 import javax.persistence.Convert;
@@ -39,6 +56,18 @@ public class DataRequestEntity {
     private Map<LeafResource, Rules> leafResourceMap;
 
     public DataRequestEntity() { }
+
+    public DataRequestEntity(final DataRequestConfig config) {
+        this.requestId = config.getOriginalRequestId().getId();
+        this.context = config.getContext();
+        this.leafResourceMap = config.getRules();
+    }
+
+    public DataRequestConfig dataRequestConfig() {
+        final DataRequestConfig request = new DataRequestConfig().user(this.user).context(this.context).rules(this.leafResourceMap);
+        request.setOriginalRequestId(new RequestId().id(this.requestId));
+        return request;
+    }
 
     public String getRequestId() {
         return requestId;
--- palisade-service/src/main/java/uk/gov/gchq/palisade/service/palisade/domain/LeafResourceRulesEntity.java
+++ palisade-service/src/main/java/uk/gov/gchq/palisade/service/palisade/domain/LeafResourceRulesEntity.java
@@ -1,5 +1,21 @@
+/*
+ * Copyright 2020 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.gov.gchq.palisade.service.palisade.domain;
 
+import uk.gov.gchq.palisade.RequestId;
 import uk.gov.gchq.palisade.resource.LeafResource;
 import uk.gov.gchq.palisade.rule.Rules;
 
@@ -10,12 +26,13 @@ import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
+import java.util.AbstractMap.SimpleImmutableEntry;
 
 @Entity
 @Table(name = "leaf_resource_rules",
         uniqueConstraints = {@UniqueConstraint(columnNames = "id")},
         indexes = {@Index(name = "request", columnList = "request_id, id"),
-                @Index(name = "resource", columnList = "id, inserted, request_id")})
+                @Index(name = "resource", columnList = "id, request_id")})
 public class LeafResourceRulesEntity {
 
     @Id
@@ -31,9 +48,20 @@ public class LeafResourceRulesEntity {
 
     @Column(name = "rules", columnDefinition = "json")
     @Convert(attributeName = "rules", converter = RulesConverter.class)
-    private Rules rules;
+    private Rules<?> rules;
 
     public LeafResourceRulesEntity() { }
+
+    public LeafResourceRulesEntity(final RequestId request, final LeafResource leafResource, final Rules<?> rules) {
+        this.requestId = request.getId();
+        this.id = leafResource.getId();
+        this.leafResource = leafResource;
+        this.rules = rules;
+    }
+
+    public SimpleImmutableEntry<LeafResource, Rules<?>> leafResourceRules() {
+        return new SimpleImmutableEntry<>(this.leafResource, this.rules);
+    }
 
     public String getId() {
         return id;
@@ -59,11 +87,11 @@ public class LeafResourceRulesEntity {
         this.leafResource = leafResource;
     }
 
-    public Rules getRules() {
+    public Rules<?> getRules() {
         return rules;
     }
 
-    public void setRules(Rules rules) {
+    public void setRules(Rules<?> rules) {
         this.rules = rules;
     }
 }
