b078946470091ff88adac0e0f3700576a80bddd0
dev930018 <dev930018@gmail.com>
2020-02-26 17:25:44 +0000

    PAL-391 removed usused import

--- palisade-service/src/main/java/uk/gov/gchq/palisade/service/palisade/request/Policy.java
+++ palisade-service/src/main/java/uk/gov/gchq/palisade/service/palisade/request/Policy.java
@@ -18,10 +18,7 @@ package uk.gov.gchq.palisade.service.palisade.request;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-import uk.gov.gchq.palisade.ToStringBuilder;
 import uk.gov.gchq.palisade.User;
 import uk.gov.gchq.palisade.resource.Resource;
 import uk.gov.gchq.palisade.rule.PredicateRule;
