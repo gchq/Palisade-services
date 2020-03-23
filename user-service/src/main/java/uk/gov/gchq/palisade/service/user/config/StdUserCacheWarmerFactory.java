b0ba499b43a515f1f7e792eec0e6d83c940dae58
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-03-23 10:34:15 +0000

    Pal 394 removed extra logging

--- user-service/src/main/java/uk/gov/gchq/palisade/service/user/config/StdUserCacheWarmerFactory.java
+++ user-service/src/main/java/uk/gov/gchq/palisade/service/user/config/StdUserCacheWarmerFactory.java
@@ -76,7 +76,6 @@ public class StdUserCacheWarmerFactory implements CacheWarmerFactory {
 
     @Override
     public User warm() {
-        System.out.println("Using the StdUser warm method");
         return new User()
                 .userId(this.getUserId())
                 .roles(this.getRoles())
