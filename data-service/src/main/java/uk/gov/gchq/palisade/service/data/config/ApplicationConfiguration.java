02e6bea41a199154f2f403871ca0311771eff021
NigelW <nigel.warburton@digital.ncsc.gov.uk>
2020-04-30 09:33:59 +0100

    PAL-416-remove config

--- data-service/src/main/java/uk/gov/gchq/palisade/service/data/config/ApplicationConfiguration.java
+++ data-service/src/main/java/uk/gov/gchq/palisade/service/data/config/ApplicationConfiguration.java
@@ -23,7 +23,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
