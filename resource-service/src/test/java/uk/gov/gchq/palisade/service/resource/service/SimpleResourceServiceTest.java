ba55a1fcf9fcbca33b8b8029c165890d843097ca
Dev 930018 <dev930018@gmail.com>
2020-05-01 15:04:35 +0100

    PAL-390 checkstyle unused import

--- resource-service/src/test/java/uk/gov/gchq/palisade/service/resource/service/SimpleResourceServiceTest.java
+++ resource-service/src/test/java/uk/gov/gchq/palisade/service/resource/service/SimpleResourceServiceTest.java
@@ -20,7 +20,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import uk.gov.gchq.palisade.resource.LeafResource;
