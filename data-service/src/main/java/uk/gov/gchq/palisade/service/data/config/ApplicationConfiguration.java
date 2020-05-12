fab7824da67fcda930e65151f1328bc2e8e07246
Dev 930018 <dev930018@gmail.com>
2020-05-12 14:07:33 +0100

    PAL-639 missing data-service import

--- data-service/src/main/java/uk/gov/gchq/palisade/service/data/config/ApplicationConfiguration.java
+++ data-service/src/main/java/uk/gov/gchq/palisade/service/data/config/ApplicationConfiguration.java
@@ -20,11 +20,11 @@ import com.netflix.discovery.EurekaClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
