867f7a59dc7d9b4db0afbbfb72eb2225fb846d52
developer01189998819991197253 <ChrisCoDev@gmail.com>
2020-06-10 15:42:37 +0100

    fixed checkstyle

--- data-service/src/main/java/uk/gov/gchq/palisade/service/data/service/AuditService.java
+++ data-service/src/main/java/uk/gov/gchq/palisade/service/data/service/AuditService.java
@@ -40,7 +40,7 @@ public class AuditService implements Service {
     }
 
     /**
-     * Calls the Audit Client and via feign sends audit requests to the Audit service returning TRUE if the audit message was received successfully. 
+     * Calls the Audit Client and via feign sends audit requests to the Audit service returning TRUE if the audit message was received successfully.
      * Audit service implementations may have different definitions of 'received successfully'.
      *
      * @param request the request
