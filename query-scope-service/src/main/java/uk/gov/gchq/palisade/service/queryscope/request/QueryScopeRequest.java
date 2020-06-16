69d307e6a458b6f4b12bc070e3633366637b5712
m78233 <m7700325@gmail.com>
2020-06-16 10:32:44 +0100

    PAL-423-New-Message-Type: second iteration on the Query Scope Service

---
+++ query-scope-service/src/main/java/uk/gov/gchq/palisade/service/queryscope/request/QueryScopeRequest.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2020 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.gov.gchq.palisade.service.queryscope.request;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import org.springframework.util.Assert;
+import uk.gov.gchq.palisade.Generated;
+import uk.gov.gchq.palisade.service.queryscope.response.common.domain.ResourceMetadata;
+import uk.gov.gchq.palisade.service.queryscope.response.common.domain.Rule;
+import uk.gov.gchq.palisade.service.queryscope.response.common.domain.User;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+/**
+ * Represents the  data that has been sent from the client to Palisade Service for a request to access data.
+ * The data will be forwarded to a set of services with each contributing to the processing of this request.
+ * This class represents the request for the Query Scope Service
+ * The next in the sequence will the response for the Query Scope Service.
+ * Note there are two class that represents the same data where each has a different purpose.
+ * uk.gov.gchq.palisade.service.policy.response.PolicyResponse is the output from the Resource Service
+ * uk.gov.gchq.palisade.service.queryscope.request.QueryScopeRequest is the input for the Query Scope Service
+ */
+@JsonDeserialize(builder = QueryScopeRequest.Builder.class)
+public final class QueryScopeRequest {
+
+    private final String token; // Unique identifier for this specific request end-to-end
+    private final User user;  // User that is making the request
+    private final Map<String, ResourceMetadata> resources; //map of resources related to this query
+    private final Map<String, String> context;  // represents the context information
+    private final Map<String, Rule> rules; // holds all of the rules applicable to this request
+
+
+    private QueryScopeRequest(String token, User user, Map<String, ResourceMetadata> resources, Map<String, String> context, Map<String, Rule> rules ) {
+        this.token = token;
+        this.user = user;
+        this.resources = resources;
+        this.context = context;
+        this.rules = rules;
+    }
+
+    @Generated
+    public String getToken() {
+        return token;
+    }
+
+    @Generated
+    public User getUser() {
+        return user;
+    }
+
+    @Generated
+    public Map<String, ResourceMetadata> getResources() {
+        return resources;
+    }
+
+    @Generated
+    public Map<String, String> getContext() {
+        return context;
+    }
+
+    @Generated
+    public Map<String, Rule> getRules() {
+        return rules;
+    }
+
+    @Override
+    @Generated
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof QueryScopeRequest)) {
+            return false;
+        }
+        QueryScopeRequest that = (QueryScopeRequest) o;
+        return token.equals(that.token) &&
+                user.equals(that.user) &&
+                resources.equals(that.resources) &&
+                context.equals(that.context) &&
+                rules.equals(that.rules);
+    }
+
+    @Override
+    @Generated
+    public int hashCode() {
+        return Objects.hash(token, user, resources, context, rules);
+    }
+
+    @Override
+    @Generated
+    public String toString() {
+        return new StringJoiner(", ", QueryScopeRequest.class.getSimpleName() + "[", "]")
+                .add("token='" + token + "'")
+                .add("user=" + user)
+                .add("resources=" + resources)
+                .add("context=" + context)
+                .add("rules=" + rules)
+                .add(super.toString())
+                .toString();
+    }
+
+    /**
+     * Builder class for the creation of instances of the QueryScopeRequest.  The variant of the Builder Pattern is
+     * meant to be used by first populating the Builder class and then us this to create the QueryScopeRequest class.
+     */
+    @JsonPOJOBuilder
+    public static class Builder {
+        private String token;
+        private User user;
+        private Map<String, ResourceMetadata> resources;
+        private Map<String, String> context;
+        private Map<String, Rule> rules;
+
+        public Builder token(String token) {
+            this.token = token;
+            return this;
+        }
+
+        public Builder userJson(User user) {
+            this.user = user;
+            return this;
+        }
+
+        public Builder resource(Map<String, ResourceMetadata> resources) {
+            this.resources = resources;
+            return this;
+        }
+
+        public Builder context(Map<String, String> context) {
+            this.context = context;
+            return this;
+        }
+
+        public Builder rules(  Map<String, Rule> rules){
+            this.rules = rules;
+            return this;
+        }
+
+
+        public QueryScopeRequest build() {
+            Assert.notNull(token, "Token Id cannot be null");
+            Assert.notNull(user, "User cannot be null");
+            Assert.notNull(resources, "Resources cannot be null");
+            Assert.notNull(resources, "Resources cannot be empty");
+            Assert.notNull(context, "Context cannot be null");
+            Assert.notEmpty(context, "Context cannot be empty");
+            Assert.notNull(rules, "Context cannot be null");
+            Assert.notEmpty(rules, "Context cannot be empty");
+            return new QueryScopeRequest(token, user, resources, context, rules);
+        }
+    }
+
+}
--- query-scope-service/src/main/java/uk/gov/gchq/palisade/service/queryscope/response/QueryScopeResponse.java
+++ query-scope-service/src/main/java/uk/gov/gchq/palisade/service/queryscope/response/QueryScopeResponse.java
@@ -1,27 +1,120 @@
+/*
+ * Copyright 2020 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.gov.gchq.palisade.service.queryscope.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import org.springframework.util.Assert;
+import uk.gov.gchq.palisade.Generated;
 import uk.gov.gchq.palisade.service.queryscope.response.common.domain.ResourceMetadata;
-import uk.gov.gchq.palisade.service.queryscope.response.common.domain.User;
 
 import java.util.Map;
+import java.util.Objects;
+import java.util.StringJoiner;
+
 
 /**
- * This is the message that will be sent from the QueryScopeService to the Results Service
- * It is, therefore a Response from the QueryScopeService and a Request into the Results Service
- * Note there are two classes of this type:
- * uk.gov.gchq.palisade.service.queryscope.requestResponse.QueryScopeResponse
- * uk.gov.gchq.palisade.service.results.request.QueryScopeResponse
+ * Represents the  data that has been sent from the client to Palisade Service for a request to access data.
+ * The data will be forwarded to a set of services with each contributing to the processing of this request.
+ * This class represents the response from the Query Scope Service
+ * The next in the sequence will the request for the Results Service.
+ * Note there are two class that represents the same data where each has a different purpose.
+ * uk.gov.gchq.palisade.service.queryscope.response.QueryScopeResponse is the output from the Query Scope Service
+ * uk.gov.gchq.palisade.service.results.request.ResultsRequest is the input for the Response Service
+ * The resources in this class will be modified from the set in the initial request to Query Scope Service.  This set
+ * will have been processed with the rules and context with the resources here representing the redacted version.
  */
+
+
+@JsonDeserialize(builder = QueryScopeResponse.Builder.class)
 public class QueryScopeResponse {
 
-    private String token; // unique identifier for this specific request end-to-end.  Was a RequestId object now a String.
-    // RequestId  represents the  Token shown in the diagram Logical view of Palisade.
-    // This information will also be in the header.  This might be removed later if not required in services.
-    //the concept of a unique identifier for each transaction is to pulled from the header
+    private final String token; // Unique identifier for this specific request end-to-end
+    private final Map<String, ResourceMetadata> resources; //masked resources related to this query
 
-    private User user;  //User replaces the user id from the original request.  This is also an indication that the User is in the system.
-    private String resourceID;  //the resource that that is being asked to access
-    private Map<String, String> context;    // relevant  information about the request.  Was a Context object now a Map.
+    private QueryScopeResponse(String token,  Map<String, ResourceMetadata> resources ) {
+        this.token = token;
+        this.resources = resources;
+    }
 
-    private Map<String, ResourceMetadata> metadata;  //this is a redacted list of metadata
+    @Generated
+    public String getToken() {
+        return token;
+    }
+
+    @Generated
+    public Map<String, ResourceMetadata> getResources() {
+        return resources;
+    }
+
+    @Override
+    @Generated
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof QueryScopeResponse)) {
+            return false;
+        }
+        QueryScopeResponse that = (QueryScopeResponse) o;
+        return token.equals(that.token) &&
+                resources.equals(that.resources);
+    }
+
+    @Override
+    @Generated
+    public int hashCode() {
+        return Objects.hash(token, resources);
+    }
+
+    @Override
+    @Generated
+    public String toString() {
+        return new StringJoiner(", ", QueryScopeResponse.class.getSimpleName() + "[", "]")
+                .add("token='" + token + "'")
+                .add("resources=" + resources)
+                .add(super.toString())
+                .toString();
+    }
+
+    /**
+     * Builder class for the creation of instances of the QueryScopeResponse.  The variant of the Builder Pattern is
+     * meant to be used by first populating the Builder class and then us this to create the QueryScopeResponse class.
+     */
+    @JsonPOJOBuilder
+    public static class Builder {
+        private String token;
+        private Map<String, ResourceMetadata> resources;
+
+        public Builder token(String token) {
+            this.token = token;
+            return this;
+        }
+
+        public Builder resource(Map<String, ResourceMetadata> resources) {
+            this.resources = resources;
+            return this;
+        }
+
+        public QueryScopeResponse build() {
+            Assert.notNull(token, "Token Id cannot be null");
+            Assert.notNull(resources, "Resources cannot be null");
+            Assert.notNull(resources, "Resources cannot be empty");
+            return new QueryScopeResponse(token, resources);
+        }
+    }
+
 }
