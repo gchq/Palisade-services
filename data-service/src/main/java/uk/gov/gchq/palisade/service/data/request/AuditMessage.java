53862c880e3184d684aee97b9c952cdf8383bada
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-07-20 11:35:50 +0100

    PAL-423, updated Serviec AuditErrorMessageTest, updated data-service AuditMessage to pull service name from yaml

--- data-service/src/main/java/uk/gov/gchq/palisade/service/data/request/AuditMessage.java
+++ data-service/src/main/java/uk/gov/gchq/palisade/service/data/request/AuditMessage.java
@@ -21,6 +21,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.Assert;
 
 import uk.gov.gchq.palisade.Context;
@@ -44,8 +45,6 @@ import java.util.StringJoiner;
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class AuditMessage {
 
-    public static final String SERVICE_NAME = "data-service";
-
     protected static final ObjectMapper MAPPER = new ObjectMapper();
 
     @JsonProperty("userId")
@@ -57,8 +56,9 @@ public class AuditMessage {
     @JsonProperty("context")
     protected final JsonNode context;   //Relevant context information about the request.
 
+    @Value("${spring.application.name}")
     @JsonProperty("serviceName")
-    protected final String serviceName;  //service that sent the message
+    protected String serviceName;  //service that sent the message
 
     @JsonProperty("timestamp")
     protected final String timestamp;  //when the message was created
@@ -71,7 +71,6 @@ public class AuditMessage {
 
     @JsonProperty("attributes")
     protected final Map<String, Object> attributes;  //Map<String, Object> holding optional extra information
-
 
     @JsonCreator
     protected AuditMessage(
@@ -89,7 +88,6 @@ public class AuditMessage {
         this.resourceId = resourceId;
         this.context = context;
 
-        this.serviceName = SERVICE_NAME;
         this.timestamp = ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT);
 
         try {
