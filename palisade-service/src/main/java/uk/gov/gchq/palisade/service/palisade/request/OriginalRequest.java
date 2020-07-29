2a85d9f46d4b1b2628e3acdc922fb0063d725597
m78233 <61839550+m78233@users.noreply.github.com>
2020-07-29 17:03:48 +0100

    PAL-423-Message-Types: changes to OriginalRequest

--- palisade-service/src/main/java/uk/gov/gchq/palisade/service/palisade/request/OriginalRequest.java
+++ palisade-service/src/main/java/uk/gov/gchq/palisade/service/palisade/request/OriginalRequest.java
@@ -18,12 +18,12 @@ package uk.gov.gchq.palisade.service.palisade.request;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.util.Assert;
 
 import uk.gov.gchq.palisade.Context;
 import uk.gov.gchq.palisade.Generated;
 
 import java.util.Objects;
+import java.util.Optional;
 import java.util.StringJoiner;
 
 /**
@@ -48,13 +48,9 @@ public final class OriginalRequest {
             final @JsonProperty("resourceId") String resourceId,
             final @JsonProperty("context") Context context) {
 
-        Assert.notNull(userId, "User cannot be null");
-        Assert.notNull(resourceId, "Resource cannot be null");
-        Assert.notNull(context, "Context cannot be null");
-
-        this.userId = userId;
-        this.resourceId = resourceId;
-        this.context = context;
+        this.userId = Optional.ofNullable(userId).orElseThrow(() -> new RuntimeException("User ID cannot be null"));
+        this.resourceId = Optional.ofNullable(resourceId).orElseThrow(() -> new RuntimeException("Resource ID  cannot be null"));
+        this.context = Optional.ofNullable(context).orElseThrow(() -> new RuntimeException("Context cannot be null"));
     }
 
     @Generated
