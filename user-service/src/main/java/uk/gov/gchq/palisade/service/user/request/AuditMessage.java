ab28314f4e113b3a4864d3aa8724d3bf417ed3d2
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-07-30 16:40:32 +0100

    PAL-423, updated AuditMessage to replace lambda with method reference when creating new HashMap

--- user-service/src/main/java/uk/gov/gchq/palisade/service/user/request/AuditMessage.java
+++ user-service/src/main/java/uk/gov/gchq/palisade/service/user/request/AuditMessage.java
@@ -83,7 +83,7 @@ public class AuditMessage {
         this.userId = Optional.ofNullable(userId).orElseThrow(() -> new RuntimeException("User ID cannot be null"));
         this.resourceId = Optional.ofNullable(resourceId).orElseThrow(() -> new RuntimeException("Resource ID  cannot be null"));
         this.context = Optional.ofNullable(context).orElseThrow(() -> new RuntimeException("Context cannot be null"));
-        this.attributes = Optional.ofNullable(attributes).orElseGet(() -> new HashMap<>());
+        this.attributes = Optional.ofNullable(attributes).orElseGet(HashMap::new);
 
         this.timestamp = ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT);
 
