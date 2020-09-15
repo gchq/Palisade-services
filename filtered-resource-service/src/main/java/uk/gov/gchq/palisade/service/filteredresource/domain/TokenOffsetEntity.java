46d22cde713ccc0721308df17a73966972f0a75d
dev930018 <dev930018@gmail.com>
2020-09-15 09:46:45 +0100

    PAL-814 added missing files

---
+++ filtered-resource-service/src/main/java/uk/gov/gchq/palisade/service/filteredresource/domain/TokenOffsetEntity.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2020 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.gov.gchq.palisade.service.filteredresource.domain;
+
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import uk.gov.gchq.palisade.Generated;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(
+        name = "token_offsets",
+        indexes = {
+                @Index(name = "message_token", columnList = "message_token", unique = true)
+        }
+)
+@RedisHash("TokenOffsetEntity")
+public class TokenOffsetEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "message_token", columnDefinition = "varchar(255)")
+    @org.springframework.data.annotation.Id
+    @Indexed
+    protected String token;
+
+    @Column(name = "commit_offset", columnDefinition = "long")
+    protected Long offset;
+
+    public TokenOffsetEntity() {
+        // no-args constructor
+    }
+
+    /**
+     * Constructor used for the Database that takes a {@link String} and EntityId
+     * Used for inserting objects into the backing store
+     *
+     * @param token  The Entity type enum object.
+     * @param offset The Id of the entity, which eventually becomes a hash of the type and Id as a primary key
+     */
+    @PersistenceConstructor
+    public TokenOffsetEntity(final String token, final Long offset) {
+        this.token = token;
+        this.offset = offset;
+    }
+
+    @Generated
+    public String getToken() {
+        return token;
+    }
+
+    @Generated
+    public Long getOffset() {
+        return offset;
+    }
+
+    @Override
+    @Generated
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof TokenOffsetEntity)) {
+            return false;
+        }
+        final TokenOffsetEntity that = (TokenOffsetEntity) o;
+        return Objects.equals(token, that.token) &&
+                Objects.equals(offset, that.offset);
+    }
+
+    @Override
+    @Generated
+    public int hashCode() {
+        return Objects.hash(token, offset);
+    }
+
+}
---
+++ filtered-resource-service/src/main/java/uk/gov/gchq/palisade/service/filteredresource/web/FilteredResourceController.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2020 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.gchq.palisade.service.filteredresource.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import uk.gov.gchq.palisade.service.filteredresource.message.FilteredResourceRequest;
+import uk.gov.gchq.palisade.service.filteredresource.message.StreamMarker;
+import uk.gov.gchq.palisade.service.filteredresource.message.Token;
+import uk.gov.gchq.palisade.service.filteredresource.message.TokenOffsetMessage;
+import uk.gov.gchq.palisade.service.filteredresource.service.ErrorReporterDaemon;
+import uk.gov.gchq.palisade.service.filteredresource.service.FilteredResourceService;
+import uk.gov.gchq.palisade.service.filteredresource.service.TokenOffsetDaemon;
+
+@RestController
+@RequestMapping("/streamApi")
+public class FilteredResourceController {
+    private final ErrorReporterDaemon errorReporterDaemon;
+    private final TokenOffsetDaemon tokenOffsetDaemon;
+    private final FilteredResourceService service;
+
+    public FilteredResourceController(final ErrorReporterDaemon errorReporterDaemon, final TokenOffsetDaemon tokenOffsetDaemon, final FilteredResourceService service) {
+        this.errorReporterDaemon = errorReporterDaemon;
+        this.tokenOffsetDaemon = tokenOffsetDaemon;
+        this.service = service;
+    }
+
+    @PostMapping("/reportError")
+    public ResponseEntity<Void> reportError(
+            final @RequestHeader(Token.HEADER) String token,
+            final @RequestBody Throwable error) {
+        errorReporterDaemon.reportError(token, error);
+
+        return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    @PostMapping("/tokenOffset")
+    public ResponseEntity<Void> storeTokenOffset(
+            final @RequestHeader(Token.HEADER) String token,
+            final @RequestBody TokenOffsetMessage offsetMessage) {
+        tokenOffsetDaemon.storeTokenOffset(token, offsetMessage.queuePointer);
+
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/acceptFilteredResource")
+    public ResponseEntity<Void> acceptFilteredResource(
+            final @RequestHeader(Token.HEADER) String token,
+            final @RequestHeader(value = StreamMarker.HEADER, required = false) StreamMarker streamMarker,
+            final @RequestBody(required = false) FilteredResourceRequest resourceRequest) {
+        service.spawnProcessorForToken(token);
+
+        return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+}
