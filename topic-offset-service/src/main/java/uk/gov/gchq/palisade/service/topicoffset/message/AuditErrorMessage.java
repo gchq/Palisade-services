ba3acaf47d5a1b9d3670ccbba47e3962c5a6aeab
m78233 <61839550+m78233@users.noreply.github.com>
2020-09-17 11:10:31 +0100

    PAL-409-TopicOffsetService: fix to AuditErrorMessage equals method

--- topic-offset-service/src/main/java/uk/gov/gchq/palisade/service/topicoffset/message/AuditErrorMessage.java
+++ topic-offset-service/src/main/java/uk/gov/gchq/palisade/service/topicoffset/message/AuditErrorMessage.java
@@ -176,7 +176,7 @@ public final class AuditErrorMessage extends AuditMessage {
             return false;
         }
         AuditErrorMessage that = (AuditErrorMessage) o;
-        return error.equals(that.error);
+        return error.getMessage().equals(that.error.getMessage());
     }
 
     @Override
