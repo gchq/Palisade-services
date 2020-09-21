d5b567537841ed3bd2491fb19b7b8f99fe9848c6
m78233 <61839550+m78233@users.noreply.github.com>
2020-09-21 09:10:02 +0100

    PAL-409-TopicOffsetService: temporarly parking contract tests in a stat that is not currently working.

---
+++ topic-offset-service/src/contract-tests/java/uk/gov/gchq/palisade/contract/topicoffset/kafka/ContractTestConfiguration.java
@@ -0,0 +1,90 @@
+package uk.gov.gchq.palisade.contract.topicoffset.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.testcontainers.containers.KafkaContainer;
+import uk.gov.gchq.palisade.Context;
+import uk.gov.gchq.palisade.User;
+import uk.gov.gchq.palisade.UserId;
+import uk.gov.gchq.palisade.resource.LeafResource;
+import uk.gov.gchq.palisade.resource.impl.FileResource;
+import uk.gov.gchq.palisade.resource.impl.SystemResource;
+import uk.gov.gchq.palisade.service.SimpleConnectionDetail;
+import uk.gov.gchq.palisade.service.topicoffset.model.StreamMarker;
+import uk.gov.gchq.palisade.service.topicoffset.model.Token;
+import uk.gov.gchq.palisade.service.topicoffset.model.TopicOffsetRequest;
+import uk.gov.gchq.palisade.service.topicoffset.model.TopicOffsetResponse;
+
+import java.time.Duration;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+public class ContractTestConfiguration {
+    public static final String REQUEST_TOKEN = "test-request-token";
+
+    public static final UserId USER_ID = new UserId().id("test-user-id");
+    public static final User USER = new User().userId(USER_ID);
+    public static final String RESOURCE_ID = "/test/resourceId";
+    public static final String RESOURCE_TYPE = "uk.gov.gchq.palisade.test.TestType";
+    public static final String RESOURCE_FORMAT = "avro";
+    public static final String DATA_SERVICE_NAME = "test-data-service";
+    public static final String RESOURCE_PARENT = "/test";
+    public static final LeafResource LEAF_RESOURCE = new FileResource()
+            .id(RESOURCE_ID)
+            .type(RESOURCE_TYPE)
+            .serialisedFormat(RESOURCE_FORMAT)
+            .connectionDetail(new SimpleConnectionDetail().serviceName(DATA_SERVICE_NAME))
+            .parent(new SystemResource().id(RESOURCE_PARENT));
+
+    public static final String PURPOSE = "test-purpose";
+    public static final Context CONTEXT = new Context().purpose(PURPOSE);
+
+    public static final String RULE_MESSAGE = "test-rule";
+
+    public static final TopicOffsetRequest REQUEST = TopicOffsetRequest.Builder.create()
+            .withUserId(USER_ID.getId())
+            .withResourceId(RESOURCE_ID)
+            .withContext(CONTEXT)
+            .withResource(LEAF_RESOURCE);
+
+
+
+    public static final ProducerRecord<String, TopicOffsetRequest> START = new ProducerRecord<>("rule", 0, null, null);
+
+    public static final ProducerRecord<String, TopicOffsetRequest> RECORD = new ProducerRecord<>("rule", 0, null, REQUEST);
+
+    public static final ProducerRecord<String, TopicOffsetRequest> END = new ProducerRecord<>("rule", 0, null, null);
+
+
+    static {
+        START.headers().add(StreamMarker.HEADER, StreamMarker.START.toString().getBytes());
+        START.headers().add(Token.HEADER, REQUEST_TOKEN.getBytes());
+
+        RECORD.headers().add(Token.HEADER, REQUEST_TOKEN.getBytes());
+
+        END.headers().add(StreamMarker.HEADER, StreamMarker.END.toString().getBytes());
+        END.headers().add(Token.HEADER, REQUEST_TOKEN.getBytes());
+    }
+
+
+    public static class TopicOffsetRequestSerialiser extends JsonSerializer<TopicOffsetRequest> {
+    }
+
+    public static class TopicOffsetResponseDeserialiser extends JsonDeserializer<TopicOffsetResponse> {
+    }
+
+    public static <R> CompletableFuture<LinkedList<ConsumerRecord<String, R>>> consumeWithTimeout(final KafkaConsumer<String, R> consumer, final Duration timeout) {
+        return CompletableFuture.supplyAsync(() -> {
+                    LinkedList<ConsumerRecord<String, R>> collected = new LinkedList<>();
+                    consumer.poll(timeout).forEach(collected::add);
+                    return collected;
+                }
+        );
+    }
+
+
+}
--- topic-offset-service/src/contract-tests/java/uk/gov/gchq/palisade/contract/topicoffset/kafka/KafkaContractTest.java
+++ topic-offset-service/src/contract-tests/java/uk/gov/gchq/palisade/contract/topicoffset/kafka/KafkaContractTest.java
@@ -23,6 +23,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.AfterAll;
@@ -30,18 +31,23 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.KafkaContainer;
+
+import uk.gov.gchq.palisade.Context;
+import uk.gov.gchq.palisade.User;
+import uk.gov.gchq.palisade.UserId;
+import uk.gov.gchq.palisade.resource.LeafResource;
+import uk.gov.gchq.palisade.resource.impl.FileResource;
+import uk.gov.gchq.palisade.resource.impl.SystemResource;
+import uk.gov.gchq.palisade.service.SimpleConnectionDetail;
 import uk.gov.gchq.palisade.service.topicoffset.TopicOffsetApplication;
+import uk.gov.gchq.palisade.service.topicoffset.model.StreamMarker;
+import uk.gov.gchq.palisade.service.topicoffset.model.Token;
 import uk.gov.gchq.palisade.service.topicoffset.model.TopicOffsetRequest;
 import uk.gov.gchq.palisade.service.topicoffset.model.TopicOffsetResponse;
-
-//import uk.gov.gchq.palisade.service.attributemask.AttributeMaskingApplication;
-//import uk.gov.gchq.palisade.service.attributemask.message.AttributeMaskingRequest;
-//import uk.gov.gchq.palisade.service.attributemask.message.AttributeMaskingResponse;
 
 import java.time.Duration;
 import java.util.Collections;
@@ -52,106 +58,97 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.gchq.palisade.contract.topicoffset.kafka.ContractTestConfiguration.END;
+import static uk.gov.gchq.palisade.contract.topicoffset.kafka.ContractTestConfiguration.RECORD;
+import static uk.gov.gchq.palisade.contract.topicoffset.kafka.ContractTestConfiguration.START;
+import static uk.gov.gchq.palisade.contract.topicoffset.kafka.ContractTestConfiguration.consumeWithTimeout;
+
 
 /**
- * An external requirement of the service is to connect to a pair of kafka topics.
- * The upstream /// topic is written to by the ///// and read by this service.
- * The downstream "/////" topic is written to by this service and read by the /////.
- * Upon writing to the upstream topic, appropriate messages should be written to the downstream topic.
- */
-@Disabled
-@SpringBootTest(classes = TopicOffsetApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("dbtest")
-class KafkaContractTest {
+     * An external requirement of the service is to connect to a pair of kafka topics.
+     * The upstream "rule" topic is written to by the policy-service and read by this service.
+     * The downstream "filtered-resource" topic is written to by this service and read by the filtered-resource-service.
+     * Upon writing to the upstream topic, appropriate messages should be written to the downstream topic.
+     */
+    @Disabled
+    @SpringBootTest(classes = TopicOffsetApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+    @ActiveProfiles("dbtest")
+    class KafkaContractTest {
 
-    public static class TopicOffsetRequestSerialiser extends JsonSerializer<TopicOffsetRequest> {
-    }
+            private static final KafkaContainer KAFKA_CONTAINER = new KafkaContainer("5.5.1");
+            private static Map<String, Object> producerProperties;
+            private static Map<String, Object> consumerProperties;
+            private static final Duration TIMEOUT_SECONDS = Duration.ofSeconds(15);
 
-    public static class TopicOffsetResponseDeserialiser extends JsonDeserializer<TopicOffsetResponse> {
-    }
+            @BeforeAll
+            static void startTestcontainers() {
+                KAFKA_CONTAINER.addEnv("KAFKA_AUTO_CREATE_TOPICS_ENABLE", "false");
+                KAFKA_CONTAINER.addEnv("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1");
+                KAFKA_CONTAINER.start();
 
-    public static <R> CompletableFuture<LinkedList<ConsumerRecord<String, R>>> consumeWithTimeout(final KafkaConsumer<String, R> consumer, final Duration timeout) {
-        return CompletableFuture.supplyAsync(() -> {
-                    LinkedList<ConsumerRecord<String, R>> collected = new LinkedList<>();
-                    consumer.poll(timeout).forEach(collected::add);
-                    return collected;
+                Map<String, Object> adminClientProperties = Map.of(
+                        AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_CONTAINER.getBootstrapServers());
+                try (AdminClient adminClient = AdminClient.create(adminClientProperties)) {
+                    List<NewTopic> newTopicsConfig = List.of(
+                            new NewTopic("rule", 3, (short) 1),
+                            new NewTopic("filtered-resource", 3, (short) 1));
+                    adminClient.createTopics(newTopicsConfig);
                 }
-        );
-    }
 
-    private static final KafkaContainer KAFKA_CONTAINER = new KafkaContainer("5.5.1");
-    private static Map<String, Object> producerProperties;
-    private static Map<String, Object> consumerProperties;
-    private static final Duration TIMEOUT_SECONDS = Duration.ofSeconds(15);
+                producerProperties = Map.of(
+                        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_CONTAINER.getBootstrapServers(),
+                        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ContractTestConfiguration.TopicOffsetRequestSerialiser.class);
+                consumerProperties = Map.of(
+                        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_CONTAINER.getBootstrapServers(),
+                        ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+                        ConsumerConfig.GROUP_ID_CONFIG, "test.group",
+                        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ContractTestConfiguration.TopicOffsetResponseDeserialiser.class);
+            }
 
-    @BeforeAll
-    static void startTestcontainers() {
-        KAFKA_CONTAINER.addEnv("KAFKA_AUTO_CREATE_TOPICS_ENABLE", "false");
-        KAFKA_CONTAINER.addEnv("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1");
-        KAFKA_CONTAINER.start();
+        public static final TopicOffsetResponse RESPONSE = TopicOffsetResponse.Builder.create().withOffset(1023L);
 
-        Map<String, Object> adminClientProperties = Map.of(
-                AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_CONTAINER.getBootstrapServers());
-        try (AdminClient adminClient = AdminClient.create(adminClientProperties)) {
-            List<NewTopic> newTopicsConfig = List.of(
-                    new NewTopic("rule", 3, (short) 1),
-                    new NewTopic("filtered-resource", 3, (short) 1));
-            adminClient.createTopics(newTopicsConfig);
+            @AfterAll
+            static void stopTestcontainers() {
+                KAFKA_CONTAINER.stop();
+            }
+
+            @Test
+            void sendAndReceiveTest() throws ExecutionException, InterruptedException {
+                // Given a message is available on the upstream topic
+                try (KafkaProducer<String, TopicOffsetRequest> producer = new KafkaProducer<>(producerProperties)) {
+                    producer.send(START);
+                    producer.send(RECORD);
+                    producer.send(END);
+                }
+
+                // When we read all messages from the downstream topic
+                LinkedList<ConsumerRecord<String, TopicOffsetResponse>> resultsList;
+                try (KafkaConsumer<String, TopicOffsetResponse> consumer = new KafkaConsumer<>(consumerProperties)) {
+                    consumer.subscribe(Collections.singletonList("filtered-resource"));
+                    resultsList = consumeWithTimeout(consumer, TIMEOUT_SECONDS).get();
+                }
+
+                // Then there are three messages: START, RESPONSE, END
+                assertThat(resultsList)
+                        .hasSize(3);
+
+                // Then the first message is a START stream marker
+                assertThat(resultsList.getFirst().headers())
+                        .isEqualTo(START.headers());
+                resultsList.removeFirst();
+
+                // Then the last message is a END stream marker
+                assertThat(resultsList.getLast().headers())
+                        .isEqualTo(END.headers());
+                resultsList.removeLast();
+
+                // Then all middle messages are mapped from request to response appropriately
+                assertThat(resultsList)
+                        .extracting(ConsumerRecord::value)
+                        .hasSize(1)
+                        .containsExactly(RESPONSE);
+            }
+
         }
-
-        producerProperties = Map.of(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_CONTAINER.getBootstrapServers(),
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
-                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, uk.gov.gchq.palisade.serivce.topicoffsetservice.kafka.KafkaContractTest.TopicOffsetRequestSerialiser.class);
-        consumerProperties = Map.of(
-                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_CONTAINER.getBootstrapServers(),
-                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
-                ConsumerConfig.GROUP_ID_CONFIG, "test.group",
-                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
-                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, uk.gov.gchq.palisade.serivce.topicoffsetservice.kafka.KafkaContractTest.TopicOffsetResponseDeserialiser.class);
-    }
-
-    @AfterAll
-    static void stopTestcontainers() {
-        KAFKA_CONTAINER.stop();
-    }
-
-    @Test
-    void sendAndReceiveTest() throws ExecutionException, InterruptedException {
-        // Given a message is available on the upstream topic
-        try (KafkaProducer<String, TopicOffsetRequest> producer = new KafkaProducer<>(producerProperties)) {
-            producer.send(ApplicationTestData.START);
-            producer.send(ApplicationTestData.RECORD);
-            producer.send(ApplicationTestData.END);
-        }
-
-        // When we read all messages from the downstream topic
-        LinkedList<ConsumerRecord<String, uk.gov.gchq.palisade.serivce.topicoffsetservice.kafka.KafkaContractTest.TopicOffsetResponseDeserialiser>> resultsList;
-        try (KafkaConsumer<String, TopicOffsetResponse> consumer = new KafkaConsumer<>(consumerProperties)) {
-            consumer.subscribe(Collections.singletonList("filtered-resource"));
-            resultsList = consumeWithTimeout(consumer, TIMEOUT_SECONDS).get();
-        }
-
-        // Then there are three messages: START, RESPONSE, END
-        assertThat(resultsList)
-                .hasSize(3);
-
-        // Then the first message is a START stream marker
-        assertThat(resultsList.getFirst().headers())
-                .isEqualTo(ApplicationTestData.START.headers());
-        resultsList.removeFirst();
-
-        // Then the last message is a END stream marker
-        assertThat(resultsList.getLast().headers())
-                .isEqualTo(ApplicationTestData.END.headers());
-        resultsList.removeLast();
-
-        // Then all middle messages are mapped from request to response appropriately
-        assertThat(resultsList)
-                .extracting(ConsumerRecord::value)
-                .hasSize(1)
-                .containsExactly(ApplicationTestData.RESPONSE);
-    }
-
-}
-
