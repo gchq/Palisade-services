c12bd039c8e39f372b8f8694ccad57e96c2a357b
dev930018 <56113485+dev930018@users.noreply.github.com>
2020-10-01 17:03:34 +0100

    Pal 411 attribute masking service (#172)
    * PAL-411 initial restful query scope service
    
    * PAL-411 attribute masking service
    
    * PAL-411 dockerfile name update
    
    * PAL-411 checkstyle unused imports
    
    * PAL-411 charts ports update
    
    * PAL-411 helmignore update
    
    * Pal 411 addressed pr review comments
    
    * Pal 411 resolve minor sq smells
    
    * PAL-411 explicit serializable rule typing
    
    * PAL-411 fixed serializable error from develop
    
    * PAL-411 fixed sq smell from develop
    
    * PAL-411 moved all tests to new style
    
    * PAL-411 health contract
    
    * PAL-411 checkstyle
    
    * PAL-411 lots of test documentation
    
    * PAL-411 setup for kafka testing
    
    * PAL-411 really fleshed out test suite
    
    * PAL-411 finished test suite
    
    * PAL-411 sonarqube code smells
    
    * PAL-411 test all by default
    
    * PAL-411 optional npe
    
    * PAL-411 missing javadoc return
    
    * PAL-411 aggregated duplicate test classes
    
    * PAL-411 unused imports
    
    * PAL-411 final param in unittests
    
    * PAL-411 error class service name
    
    * PAL-411 sonarqube code smells
    
    * Update AuthorisedRequestEntity.java
    
    * PAL-411 remaining query-scope -> attribute-masking replacements
    
    * Update deployment.yaml
    
    * Update and rename RestContractTest.java to StreamApiContractTest.java
    
    * PAL-411 optional orelse requiring nonnull
    
    * PAL-411 updates for PAL-710
    
    * PAL-411 Akka streams
    
    * PAL-411 working tests
    
    * PAL-411 tweaks, fixes, checkstyle
    
    * PAL-411 error testing
    
    * PAL-411 some bugfixes and some sq smells
    
    * PAL-411 unneccessary vim markers
    
    * PAL-411 renamed package path names
    
    * PAL-411 consumer resets to earliest
    
    * PAL-411 sonarqube code smells on lack of documentations
    
    * PAL-411 sonarqube code smells
    
    * PAL-411 big refactor for better architecture
    
    * PAL-411 removed service iface in favour of concrete impl
    
    * PAL-411 correct method annotation
    
    * PAL-411 correct method annotation
    
    * PAL-411 checkstyle missing docs
    
    * PAL-411 update restcontroller docs
    
    * PAL-411 config problems
    
    * PAL-411 config problems
    
    * PAL-411 fixed partition assignment configuration
    
    * PAL-411 concrete static contract test data
    
    * PAL-411 sonarqube code smells
    
    * PAL-411 rest controller writes directly to queue
    
    * PAL-411 start of some statefulset conversions
    
    * PAL-411 statefulset attribute masking service
    
    * PAL-411 readme update
    
    * PAL-411 better test setup for restController akka stream
    
    * PAL-411 sonarqube code smells
    
    * PAL-411 removed requireNonNulls
    
    * PAL-411 headless service fixes, token-partition conversion and executors
    
    * PAL-411 increased jenkins helm timeout and fixed missing executor in tests
    
    * PAL-411 Token::toPartition documentation
    
    * PAL-411 more headless service fixes
    
    * PAL-411 logging and producer topic partitions
    
    * PAL-411 consistent yaml style
    
    * PAL-411 consistent yaml style
    
    * PAL-411 unused imports
    
    * PAL-411 removed recalculation of message partition
    
    * PAL-411 moved logger to service
    
    * PAL-411 bugfixes after changing logging
    
    Co-authored-by: pd104923 <54805408+pd104923@users.noreply.github.com>
    Co-authored-by: ac74475 <ac74475@users.noreply.github.com>

---
+++ attribute-masking-service/src/main/java/uk/gov/gchq/palisade/service/attributemask/web/AttributeMaskingRestController.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2020 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.gov.gchq.palisade.service.attributemask.web;
+
+import akka.Done;
+import akka.stream.Materializer;
+import akka.stream.javadsl.Keep;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import uk.gov.gchq.palisade.service.attributemask.model.AttributeMaskingRequest;
+import uk.gov.gchq.palisade.service.attributemask.model.Token;
+import uk.gov.gchq.palisade.service.attributemask.stream.ConsumerTopicConfiguration;
+import uk.gov.gchq.palisade.service.attributemask.stream.ProducerTopicConfiguration.Topic;
+
+import java.nio.charset.Charset;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+import java.util.stream.Collectors;
+
+/**
+ * A REST interface mimicking the Kafka API to the service.
+ * POSTs to the controller write the request and headers to the upstream topic.
+ * These messages will then later be read by the service.
+ * Intended for debugging only.
+ */
+@RestController
+@RequestMapping(path = "/api")
+public class AttributeMaskingRestController {
+    private final Sink<ProducerRecord<String, AttributeMaskingRequest>, CompletionStage<Done>> upstreamSink;
+    private final ConsumerTopicConfiguration upstreamConfig;
+    private final Materializer materializer;
+
+    /**
+     * Autowired constructor for the rest controller
+     *
+     * @param upstreamSink   a sink to the upstream topic
+     * @param upstreamConfig the config for the topic (name, partitions, ...)
+     * @param materializer   the akka system materializer
+     */
+    public AttributeMaskingRestController(
+            final Sink<ProducerRecord<String, AttributeMaskingRequest>, CompletionStage<Done>> upstreamSink,
+            final ConsumerTopicConfiguration upstreamConfig,
+            final Materializer materializer) {
+        this.upstreamSink = upstreamSink;
+        this.upstreamConfig = upstreamConfig;
+        this.materializer = materializer;
+    }
+
+    /**
+     * REST endpoint for debugging the service, mimicking the Kafka API.
+     *
+     * @param headers a multi-value map of http request headers
+     * @param request the (optional) request itself
+     * @return the response from the service, or an error if one occurred
+     */
+    @PostMapping(value = "/mask", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Void> maskAttributes(
+            final @RequestHeader MultiValueMap<String, String> headers,
+            final @RequestBody(required = false) AttributeMaskingRequest request) {
+        // Process request as singleton list
+        this.maskAttributesMulti(headers, Collections.singletonList(request));
+
+        // Return result
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    /**
+     * REST endpoint for debugging the service, mimicking the Kafka API.
+     * Takes a list of requests and processes each of them with the given headers
+     *
+     * @param headers  a multi-value map of http request headers
+     * @param requests a list of requests
+     * @return the response from the service, or an error if one occurred
+     */
+    @PostMapping(value = "/mask/multi", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Void> maskAttributesMulti(
+            final @RequestHeader MultiValueMap<String, String> headers,
+            final @RequestBody List<AttributeMaskingRequest> requests) {
+        // Get token from headers
+        String token = Optional.ofNullable(headers.get(Token.HEADER))
+                .orElseThrow(() -> new NoSuchElementException("No token specified in headers"))
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Token specified in headers mapped to empty list"));
+
+        // Get topic and calculate partition, unless this service has been assigned a partition
+        Topic topic = this.upstreamConfig.getTopics().get("input-topic");
+        int partition = Optional.ofNullable(topic.getAssignment())
+                .orElseGet(() -> Token.toPartition(token, topic.getPartitions()));
+
+        // Convert headers to kafka style
+        List<Header> kafkaHeaders = headers.entrySet().stream()
+                .flatMap(entries -> entries.getValue().stream().map(value -> new RecordHeader(entries.getKey(), value.getBytes(Charset.defaultCharset()))))
+                .collect(Collectors.toList());
+
+        // Process requests
+        // Akka reactive streams can't have null elements, so map to and from optional
+        Source.fromJavaStream(() -> requests.stream().map(Optional::ofNullable))
+                .map(request -> new ProducerRecord<String, AttributeMaskingRequest>(topic.getName(), partition, null, request.orElse(null), kafkaHeaders))
+                .toMat(this.upstreamSink, Keep.right())
+                .run(this.materializer)
+                .toCompletableFuture()
+                .join();
+
+        // Return results
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+}
