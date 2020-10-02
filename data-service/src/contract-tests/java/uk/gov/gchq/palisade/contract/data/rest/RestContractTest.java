7cb12ac54a4c0e157feec85999de3ddf19bed3fd
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-10-02 12:13:29 +0100

    removed one unused import

--- data-service/src/contract-tests/java/uk/gov/gchq/palisade/contract/data/rest/RestContractTest.java
+++ data-service/src/contract-tests/java/uk/gov/gchq/palisade/contract/data/rest/RestContractTest.java
@@ -21,7 +21,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import feign.Response;
-import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
