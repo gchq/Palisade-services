8c64d3a2937d846f7a2c32c21ba07d5f4bcf87fc
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-10-02 11:43:19 +0100

    more checkstyle

--- resource-service/src/component-tests/java/uk/gov/gchq/palisade/component/resource/repository/JpaPersistenceLayerTest.java
+++ resource-service/src/component-tests/java/uk/gov/gchq/palisade/component/resource/repository/JpaPersistenceLayerTest.java
@@ -22,8 +22,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
