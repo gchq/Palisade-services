5fabad649fade5f24b77ef5777b3ea3314628f74
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-10-08 19:23:19 +0100

    removed unused import, updated ResourceComponentTest

--- policy-service/src/unit-tests/java/uk/gov/gchq/palisade/service/policy/request/SetResourcePolicyRequestTest.java
+++ policy-service/src/unit-tests/java/uk/gov/gchq/palisade/service/policy/request/SetResourcePolicyRequestTest.java
@@ -20,7 +20,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.json.JsonTest;
 
 import uk.gov.gchq.palisade.User;
 import uk.gov.gchq.palisade.policy.HasTestingPurpose;
