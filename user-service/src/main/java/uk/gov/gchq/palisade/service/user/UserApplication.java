68ef2194389af7543eb6166fac8e7ff2c4ab1f56
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-10-20 17:33:05 +0100

    updated UserApplication, added javadoc

--- user-service/src/main/java/uk/gov/gchq/palisade/service/user/UserApplication.java
+++ user-service/src/main/java/uk/gov/gchq/palisade/service/user/UserApplication.java
@@ -24,12 +24,20 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Application entrypoint and main process runner
+ */
 @EnableDiscoveryClient
 @EnableCaching
 @SpringBootApplication
 public class UserApplication {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserApplication.class);
 
+    /**
+     * Application entrypoint, creates and runs a spring application, passing in the given command-line args
+     *
+     * @param args command-line arguments passed to the application
+     */
     public static void main(final String[] args) {
         LOGGER.debug("UserApplication started with: {} {} {}", UserApplication.class, "main", args);
         new SpringApplicationBuilder(UserApplication.class).web(WebApplicationType.SERVLET)
