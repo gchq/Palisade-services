a4678c18425efa820b2f69f87cefcb561da0211c
dev930018 <dev930018@gmail.com>
2020-10-26 13:28:06 +0000

    PAL-409 updated controller component test

--- topic-offset-service/src/component-tests/java/uk/gov/gchq/palisade/component/topicoffset/web/TopicOffsetRestControllerTest.java
+++ topic-offset-service/src/component-tests/java/uk/gov/gchq/palisade/component/topicoffset/web/TopicOffsetRestControllerTest.java
@@ -22,7 +22,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
@@ -34,17 +33,13 @@ import uk.gov.gchq.palisade.service.topicoffset.model.StreamMarker;
 import uk.gov.gchq.palisade.service.topicoffset.model.Token;
 import uk.gov.gchq.palisade.service.topicoffset.model.TopicOffsetRequest;
 import uk.gov.gchq.palisade.service.topicoffset.model.TopicOffsetResponse;
-import uk.gov.gchq.palisade.service.topicoffset.service.ErrorHandlingService;
-import uk.gov.gchq.palisade.service.topicoffset.service.TopicOffsetService;
-import uk.gov.gchq.palisade.service.topicoffset.stream.ConsumerTopicConfiguration;
-import uk.gov.gchq.palisade.service.topicoffset.stream.ProducerTopicConfiguration.Topic;
+import uk.gov.gchq.palisade.service.topicoffset.service.KafkaProducerService;
 import uk.gov.gchq.palisade.service.topicoffset.stream.config.AkkaSystemConfig;
 import uk.gov.gchq.palisade.service.topicoffset.web.TopicOffsetRestController;
 
-import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -64,12 +59,8 @@ class TopicOffsetRestControllerTest {
     @Autowired
     private TopicOffsetRestController controller;
 
-    @SpyBean
-    ConsumerTopicConfiguration topicConfiguration;
     @MockBean
-    private TopicOffsetService topicOffsetService;
-    @MockBean
-    private ErrorHandlingService errorHandlingService;
+    private KafkaProducerService serviceMock;
 
     public static final String SERVICE_ENDPOINT_URL = "/api/offset";
     public static final String TEST_REQUEST_TOKEN = "test-request-token";
@@ -77,15 +68,13 @@ class TopicOffsetRestControllerTest {
 
     @BeforeEach
     void setUp() {
-        Topic topic = new Topic();
-        topic.setPartitions(1);
-        topic.setName("input-topic");
-        Mockito.when(topicConfiguration.getTopics()).thenReturn(Collections.singletonMap("input-topic", topic));
+        Mockito.when(serviceMock.topicOffsetMulti(Mockito.any(), Mockito.any()))
+                .thenReturn(CompletableFuture.completedFuture(null));
     }
 
     @AfterEach
     void tearDown() {
-        Mockito.reset(topicConfiguration);
+        Mockito.reset(serviceMock);
     }
 
     @Test
@@ -97,14 +86,13 @@ class TopicOffsetRestControllerTest {
     /**
      * This permutation is for a request that comes in for an end of messages indicator, StreamMarker is set to START.
      * Test should accept an incoming request including {@link TopicOffsetRequest}, {@link Token}, and a {@link StreamMarker}
-     * as a POST to the the URL "/stream-api/topicOffset" as a JSON String. Response should be a 2XX status and null body.
+     * as a POST to the the URL "/api/offset" as a JSON String. Response should be a 2XX status and null body.
      *
      * @throws Exception if it fails to process the request
      */
     @Test
     void testControllerWithOffsetForTopic() throws Exception {
         // Given this is an offset for the topic
-        Mockito.when(topicOffsetService.isOffsetForTopic(any())).thenReturn(true);
 
         // When a request is made to the service
         HttpHeaders headers = new HttpHeaders();
@@ -115,34 +103,7 @@ class TopicOffsetRestControllerTest {
                 .headers(headers));
 
         // Then it is accepted by the rest controller
-        action.andExpect(status().isAccepted()) //a HTTP 2XX
-                .andDo(print())
-                .andExpect(jsonPath(MESSAGE_BODY_KEY).doesNotExist()) // no message
-                .andReturn();
-    }
-
-    /**
-     * This permutation is for a request that comes in for an end of messages indicator, StreamMarker is set to END.
-     * Test should accept an incoming request including TopicOffsetRequest, Token, and a StreamMarker as a Post to
-     * the the URL "/stream-api/topicOffset" as a Json String. Response should be a null.
-     *
-     * @throws Exception if it fails to process the request
-     */
-    @Test
-    void testControllerWithEndMessage() throws Exception {
-        // Given this is not an offset for the topic
-        Mockito.when(topicOffsetService.isOffsetForTopic(any())).thenReturn(false);
-
-        // When  a request is made to the service
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(Token.HEADER, TEST_REQUEST_TOKEN);
-        headers.add(StreamMarker.HEADER, StreamMarker.END.toString());
-        ResultActions action = this.mockMvc.perform(post(SERVICE_ENDPOINT_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .headers(headers));
-
-        // Then it is accepted by the rest controller
-        action.andExpect(status().isAccepted()) //a HTTP 2XX
+        action.andExpect(status().isAccepted()) // a HTTP 2XX
                 .andDo(print())
                 .andExpect(jsonPath(MESSAGE_BODY_KEY).doesNotExist()) // no message
                 .andReturn();
