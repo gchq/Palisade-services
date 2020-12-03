4a7f504f12a461372a6698530114ddcdb7c10979
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-12-03 16:20:12 +0000

    Pal 864 remmoved h2 profile yaml, updated health actuator test

--- palisade-service/src/contract-tests/java/uk/gov/gchq/palisade/contract/palisade/rest/HealthActuatorContractTest.java
+++ palisade-service/src/contract-tests/java/uk/gov/gchq/palisade/contract/palisade/rest/HealthActuatorContractTest.java
@@ -37,7 +37,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(
         classes = {PalisadeApplication.class},
         webEnvironment = WebEnvironment.RANDOM_PORT,
-        properties = {"management.health.kafka.enabled=false", "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration"}
+        properties = {"management.health.kafka.enabled=false"}
 )
 @ActiveProfiles("akka-test")
 class HealthActuatorContractTest {
