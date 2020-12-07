088680ef8c4ff0b14536399c708edb91c2096f06
pd104923 <54805408+pd104923@users.noreply.github.com>
2020-12-07 09:00:33 +0000

    Pal 864 updated component tests, added auditable test class

--- palisade-service/src/component-tests/java/uk/gov/gchq/palisade/component/palisade/model/PalisadeResponseTest.java
+++ palisade-service/src/component-tests/java/uk/gov/gchq/palisade/component/palisade/model/PalisadeResponseTest.java
@@ -46,7 +46,7 @@ class PalisadeResponseTest {
      *                     This equates to a failure to seralise or deseralise the string.
      */
     @Test
-    void testGroupedDependantErrorMessageSerialisingAndDeserialising() throws IOException {
+    void testPalisadeResponseSerialisingAndDeserialising() throws IOException {
         PalisadeResponse palisadeResponse = new PalisadeResponse("tokenID");
 
         JsonContent<PalisadeResponse> responseJsonContent = jsonTester.write(palisadeResponse);
@@ -54,14 +54,14 @@ class PalisadeResponseTest {
         ObjectContent<PalisadeResponse> responseObjectContent = jsonTester.parse(responseJsonContent.getJson());
         PalisadeResponse palisadeResponseObject = responseObjectContent.getObject();
 
-        assertAll("AuditSerialisingDeseralisingAndComparison",
-                () -> assertAll("AuditSerialisingComparedToString",
+        assertAll("Palisade Response Serialising and Deseralising Comparison",
+                () -> assertAll("Palisade Response Serialising Compared To String",
                         () -> assertThat(responseJsonContent).extractingJsonPathStringValue("$.token").isEqualTo("tokenID")
                 ),
-                () -> assertAll("AuditDeserialisingComparedToObject",
+                () -> assertAll("Palisade Response Deserialising Compared To Object",
                         () -> assertThat(palisadeResponseObject.getToken()).isEqualTo(palisadeResponse.getToken())
                 ),
-                () -> assertAll("ObjectComparison",
+                () -> assertAll("Object Comparison",
                         //compares the two objects using the objects equal method
                         () -> assertThat(palisadeResponseObject).usingRecursiveComparison().isEqualTo(palisadeResponse)
                 )
