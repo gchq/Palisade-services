9d31d91a76b97be85ffe267dc6c9921b6dc9259c
m78233 <61839550+m78233@users.noreply.github.com>
2020-12-15 07:23:59 +0000

    PAL-866_Policy_Service_Audit_Error_Handling: code clean-up
    Signed-off-by: m78233 <61839550+m78233@users.noreply.github.com>

--- policy-service/src/contract-tests/java/uk/gov/gchq/palisade/contract/policy/rest/HealthActuatorContractTest.java
+++ policy-service/src/contract-tests/java/uk/gov/gchq/palisade/contract/policy/rest/HealthActuatorContractTest.java
@@ -24,7 +24,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import uk.gov.gchq.palisade.service.policy.PolicyApplication;
 
