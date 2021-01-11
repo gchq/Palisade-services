1f55841cae5943c780c1992204279834883b03ec
kg981167224 <68223404+kg981167224@users.noreply.github.com>
2021-01-11 10:40:12 +0000

    Limit retry to stop hanging

--- create-kafka-queues/create-topics.sh
+++ create-kafka-queues/create-topics.sh
@@ -26,16 +26,16 @@
 #REPLICATION: The replication-factor to associate with this topic
 #e.g write_to_kafka palisade 1 1
 write_to_kafka () {
-  read -r NAME <<< "$1"
-  read -r PARTITION <<< "$2"
-  read -r REPLICATION <<< "$3"
+  read -r NAME <<< $1
+  read -r PARTITION <<< $2
+  read -r REPLICATION <<< $3
 
   attempts=0
   echo ./bin/kafka-topics.sh --create --replication-factor "$REPLICATION" --partitions "$PARTITION" --zookeeper "$ZOOKEEPER" --topic "$NAME"
   until ./bin/kafka-topics.sh --create --replication-factor "$REPLICATION" --partitions "$PARTITION" --zookeeper "$ZOOKEEPER" --topic "$NAME"; do
     ((attempts=attempts+1))
     if [ "$attempts" -ge 2 ]; then
-      printf "Failed after %s attempts, topics may have been created OK." $attempts
+      printf "Failed after %s attempts, topics may have been created OK.\n" $attempts
       break
     fi
     echo Retrying attempt "$attempts" creation of topic "$NAME" "$PARTITION" "$REPLICATION"
