3ecb96255aefe22a688c3fdcc9d3e2af3ace18b6
m78233 <61839550+m78233@users.noreply.github.com>
2021-01-26 08:14:58 +0000

    PAL-863-data-service-audit-message: changes to model package for the 2nd iteration of changes
    Signed-off-by: m78233 <61839550+m78233@users.noreply.github.com>

---
+++ data-service/src/main/java/uk/gov/gchq/palisade/service/data/model/TokenMessagePair.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018-2021 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.gov.gchq.palisade.service.data.model;
+
+import akka.japi.Pair;
+
+public class TokenMessagePair  extends Pair<String, AuditMessage>{
+    /**
+     * The type Token request pair.
+     */
+
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * Instantiates a new Token request pair.
+         *
+         * @param token           the token
+         * @param auditMessage the original request
+         */
+        public TokenMessagePair(final String token, final AuditMessage auditMessage) {
+            super(token, auditMessage);
+        }
+}
