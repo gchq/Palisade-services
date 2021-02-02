a482dc958d0c6e278732d6b643e2f1641c2e5b21
dev930018 <dev930018@gmail.com>
2021-02-02 10:54:07 +0000

    PAL-490 unused imports

--- resource-service/src/main/resources/schema.sql
+++ resource-service/src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2021 Crown Copyright
+ * Copyright 2021 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,67 +16,26 @@
 
 CREATE TABLE IF NOT EXISTS completeness
 (
-    `id`
-    INTEGER
-    PRIMARY
-    KEY,
-    `entity_type`
-    INTEGER,
-    `entity_id`
-    VARCHAR
-(
-    255
-)
-    );
+    `id`          INTEGER PRIMARY KEY,
+    `entity_type` INTEGER,
+    `entity_id`   VARCHAR(255)
+);
 CREATE TABLE IF NOT EXISTS resources
 (
-    `id`
-    INTEGER
-    PRIMARY
-    KEY
-    AUTO_INCREMENT,
-    `resource_id`
-    VARCHAR
-(
-    255
-) UNIQUE NOT NULL,
-    `parent_id` VARCHAR
-(
-    255
-),
-    `resource` VARCHAR
-    );
+    `id`          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `resource_id` VARCHAR(255) UNIQUE NOT NULL,
+    `parent_id`   VARCHAR(255),
+    `resource`    VARCHAR
+);
 CREATE TABLE IF NOT EXISTS types
 (
-    `id`
-    INTEGER
-    PRIMARY
-    KEY
-    AUTO_INCREMENT,
-    `resource_id`
-    VARCHAR
-(
-    255
-) UNIQUE NOT NULL,
-    `type` VARCHAR
-(
-    255
-)
-    );
+    `id`          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `resource_id` VARCHAR(255) UNIQUE NOT NULL,
+    `type`        VARCHAR(255)
+);
 CREATE TABLE IF NOT EXISTS serialised_formats
 (
-    `id`
-    INTEGER
-    PRIMARY
-    KEY
-    AUTO_INCREMENT,
-    `resource_id`
-    VARCHAR
-(
-    255
-) UNIQUE NOT NULL,
-    `serialised_format` VARCHAR
-(
-    255
-)
-    );
+    `id`                INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `resource_id`       VARCHAR(255) UNIQUE NOT NULL,
+    `serialised_format` VARCHAR(255)
+);
